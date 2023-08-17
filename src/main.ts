3651e72e5f96e0b6fe0e3c96805b8ef504088e0d
choi ha young <gkdudk15@naver.com>
2023-08-17 15:48:20 +0900

    핫리로드 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { Post } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +9,9 @@ async function bootstrap() {
   await app.listen(port);
   console.log(`listening on port ${port}`);
   
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
